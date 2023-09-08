f4ec64e036d2c5c690d69c7450a7a76986e34cb7
Владимир Тютин <vladimir30061517@gmail.com>
2023-09-08 15:50:38 +0300

    Ver_1

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,11 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Birthday
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
 class BirthdayForm(forms.ModelForm):
     class Meta:
         model = Birthday
         fields = '__all__'
-        widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
-        }
+        widgets = {'birthday': forms.DateInput(attrs={'type': 'date'}),}
+
+    def clean(self):
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
